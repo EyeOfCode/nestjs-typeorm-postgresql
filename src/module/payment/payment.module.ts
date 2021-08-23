d1839d714e037a4b57ec78cd8300e6b0fde20fe7
Champ <champuplove@gmail.com>
2021-08-23 14:49:11 +0700

    Update remove todo api exp payment

--- src/module/payment/payment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentResolver } from './payment.resolver';
-import { PaymentService } from './payment.service';
-
-@Module({
-  providers: [PaymentResolver, PaymentService],
-  exports: [PaymentService],
-})
-export class PaymentModule {}
